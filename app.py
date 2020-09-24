2c0f560e50f7f1850278b46400057f1d7a08c91c
zijingng <zijingng@gmail.com>
2020-09-24 22:57:15 +0800

    新增对临时会话的支持

--- app.py
+++ app.py
@@ -22,36 +22,59 @@ qqClient = GraiaMiraiApplication(
 )
 
 qqGroup = None
+qqId = None
 
 @bcc.receiver("GroupMessage")
 async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
-    print(message.asDisplay())
+    #print(message.asDisplay())
     global qqGroup
-    qqGroup = group
     if message.asDisplay()[0] in ['!','.','=','&','?','^']:
         #yield from client.send_message(message.channel, '%s wants his !lg' % nick)
         #yield irc_client.message('##kramell', '%s used !lg in discord channel %s' % (nick, message.channel))
         # '!RELAY -n 1 -channel ' + (pm ? 'msg' : chan) + ' -nick ' + nick + ' -prefix ' + chan + ':' + ' ' + message
-        forsequell = '!RELAY -n 1 -channel msg -nick %s -prefix qq:%s: %s' % (member.id, group.id, message.asDisplay())
-        print(forsequell)
+        forsequell = '!RELAY -n 1 -channel msg -nick %s -prefix group:%s:%s: %s' % (member.id, group.id, member.id, message.asDisplay())
+        #print(forsequell)
         await ircClient.message('Sequell', forsequell)
     
     if message.asDisplay().startswith('@?'):
+        qqGroup = group
+        qqId = None
         await ircClient.message('Gretell', message.asDisplay())
     
     if message.asDisplay().startswith('%'):
+        qqGroup = group
+        qqId = None
+        await ircClient.message('Cheibriados', message.asDisplay())
+
+@bcc.receiver("TempMessage")
+async def group_message_handler(app: GraiaMiraiApplication, message: MessageChain, group: Group, member: Member):
+    global qqGroup
+    global qqId
+    if message.asDisplay()[0] in ['!','.','=','&','?','^']:
+        forsequell = '!RELAY -n 1 -channel msg -nick %s -prefix temp:%s:%s: %s' % (member.id, group.id, member.id, message.asDisplay())
+        await ircClient.message('Sequell', forsequell)
+    
+    if message.asDisplay().startswith('@?'):
+        qqGroup = group
+        qqId = member.id
+        await ircClient.message('Gretell', message.asDisplay())
+    
+    if message.asDisplay().startswith('%'):
+        qqGroup = group
+        qqId = member.id
         await ircClient.message('Cheibriados', message.asDisplay())
 
 import pydle
 import re
 import traceback
 import sys
-#import aiohttp
 
 clrstrip = re.compile("\x03(?:\d{1,2}(?:,\d{1,2})?)?", re.UNICODE)
 
 class MyIrcClient(pydle.Client):
     async def on_message(self, target, source, message):
+        global qqGroup
+        global qqId
         try:
             print(message.encode('utf-8'))
             await super().on_message(target, source, message)
@@ -59,54 +82,56 @@ class MyIrcClient(pydle.Client):
             message = clrstrip.sub('', message)
             
             if source=='Sequell':
-            	msgarray = message.split(':')
-            	serv = msgarray[0]
-            	chanid = msgarray[1]
-            	msg = ':'.join(msgarray[2:])
-            	
-            	url_regex = '(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
-            	#msg_urls = re.findall(url_regex, msg)
-            	msg_split = re.split(url_regex, msg)
-            	
-            	for mdchar in ['\\','*','_','~','`']:
-            	    for i in range(0,len(msg_split),2):
-            	        msg_split[i] = msg_split[i].replace(mdchar,'\\'+mdchar)
-            	    #msg_wo_ulrs = msg_wo_ulrs.replace(mdchar,'\\'+mdchar)
-            	
-            	#msg = msg_wo_ulrs.format(*msg_urls)
-            	msg = ''.join(msg_split)
-            	
-            	if msg[:3]=='/me':
-            	    msg = '*'+msg[3:].strip()+'*'
-            	#msg = msg.replace('/me','*'+client.user.name+'*')
-            	
-            	if re.search('\[\d\d?/\d\d?\]:', msg):
-            	    s = re.split('(\[\d\d?/\d\d?\]:)', msg)
-            	    #msg = s[0] + s[1] + '```\n' + ''.join(s[2:]).strip() + '\n```' # put only the content of the ?? in a block
-            	    msg = s[0] + s[1] + '\n' + ''.join(s[2:]).strip()
-            	else:
-            	    #msg = '```\n' + msg + '\n```' # put in a code block to preserve formatting
-            	    msg = msg
-            	if serv=='qq':
-                    global qqGroup
-                    response = [Plain(msg)]
-                    img_url_regex = '(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\.(?:jpg|png|gif))'
-                    for url in re.findall(img_url_regex, msg):
-                        #url = url.replace('http://i.imgur.com', 'https://i.imgur.com')
-                        #async def download(url):
-                            #async with aiohttp.ClientSession() as session:
-                                #async with session.get(url) as resp:
-                                    #return await resp.content.read()
-                        #img = await download(url)
-                        #response.append(Image.fromUnsafeBytes(img))
-                        response.append(Image.fromNetworkAddress(url=url))
+                msgarray = message.split(':')
+                serv = msgarray[0]
+                group = msgarray[1]
+                member = msgarray[2]
+                msg = ':'.join(msgarray[3:])
+                
+                url_regex = '(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
+                #msg_urls = re.findall(url_regex, msg)
+                msg_split = re.split(url_regex, msg)
+                
+                for mdchar in ['\\','*','_','~','`']:
+                    for i in range(0,len(msg_split),2):
+                        msg_split[i] = msg_split[i].replace(mdchar,'\\'+mdchar)
+                    #msg_wo_ulrs = msg_wo_ulrs.replace(mdchar,'\\'+mdchar)
+                
+                #msg = msg_wo_ulrs.format(*msg_urls)
+                msg = ''.join(msg_split)
+                
+                if msg[:3]=='/me':
+                    msg = '*'+msg[3:].strip()+'*'
+                #msg = msg.replace('/me','*'+client.user.name+'*')
+                
+                if re.search('\[\d\d?/\d\d?\]:', msg):
+                    s = re.split('(\[\d\d?/\d\d?\]:)', msg)
+                    #msg = s[0] + s[1] + '```\n' + ''.join(s[2:]).strip() + '\n```' # put only the content of the ?? in a block
+                    msg = s[0] + s[1] + '\n' + ''.join(s[2:]).strip()
+                else:
+                    #msg = '```\n' + msg + '\n```' # put in a code block to preserve formatting
+                    msg = msg
+                
+                response = [Plain(msg)]
+                img_url_regex = '(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\.(?:jpg|png|gif))'
+                for url in re.findall(img_url_regex, msg):
+                    response.append(Image.fromNetworkAddress(url=url))
                         
-                    await qqClient.sendGroupMessage(qqGroup, MessageChain.create(response))
+                if serv == 'group':
+                    await qqClient.sendGroupMessage(group, MessageChain.create(response))
+                elif serv == 'temp':
+                    await qqClient.sendTempMessage(group, member, MessageChain.create(response))
             if source=='Gretell':
-                await qqClient.sendGroupMessage(qqGroup, MessageChain.create([Plain(message)]))
+                if qqId is None:
+                    await qqClient.sendGroupMessage(qqGroup, MessageChain.create([Plain(message)]))
+                else:
+                    await qqClient.sendTempMessage(qqGroup, qqId, MessageChain.create([Plain(message)]))
 
             if source=='Cheibriados':
-                await qqClient.sendGroupMessage(qqGroup, MessageChain.create([Plain(message)]))
+                if qqId is None:
+                    await qqClient.sendGroupMessage(qqGroup, MessageChain.create([Plain(message)]))
+                else:
+                    await qqClient.sendTempMessage(qqGroup, qqId, MessageChain.create([Plain(message)]))
 
         except Exception:
             print("Exception irc thread:")
